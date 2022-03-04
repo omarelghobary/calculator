bf56d551bac86a2119133a592a50705ae9dc6f01
Omar <omelghobary@gmail.com>
2022-03-05 01:42:40 +0200

    added keypad numbers functionality

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+// select buttons 
+const numberButtons = document.querySelectorAll('.number');
+const operationButtons = document.querySelectorAll('.operation');
+const equalsButton = document.querySelector('.equals');
+const clearButton = document.querySelector('.clear');
+const screen = document.querySelector('.display .screen')
+
+for (let i = 0; i < 11; i++){
+    numberButtons[i].addEventListener('click', () => 
+    appendNumber(numberButtons[i].textContent));   
+}
+
+
+function appendNumber(number){
+    if (screen.textContent === "0")
+        clearScreen();
+    screen.textContent += number;
+}
+
+function equals (){
+
+}
+
+function operate (operator, num1,num2){
+
+}
+
+function clearScreen(){
+    screen.textContent = "";
+}
+
+
 function add(a,b) {
     return a+b;
 }
@@ -14,27 +46,27 @@ function divide(a,b) {
     return a/b;
 }
  
-let a = +prompt("enter first number");
-let b = +prompt("enter second number ");
-let operator = prompt("choose type of operation + - * or /");
-let result = 0;
+// let a = +prompt("enter first number");
+// let b = +prompt("enter second number ");
+// let operator = prompt("choose type of operation + - * or /");
+// let result = 0;
 
-switch (operator){
-    case "+":
-        result = add(a,b);
-        break;
-    case "-":
-        result = subtract(a,b);
-        break;
-    case "*":
-        result = multiply(a,b);
-        break;
-    case "/":
-        result =divide(a,b);
-        break;
-    default:
-        alert("you entered wrong operator make sure to use + - * or / operators");
-}
-if (operator === "+" || operator === "-"|| operator === "*" || operator === "/")
-alert(`${a} ${operator} ${b} = ${result}`); 
+// switch (operator){
+//     case "+":
+//         result = add(a,b);
+//         break;
+//     case "-":
+//         result = subtract(a,b);
+//         break;
+//     case "*":
+//         result = multiply(a,b);
+//         break;
+//     case "/":
+//         result =divide(a,b);
+//         break;
+//     default:
+//         alert("you entered wrong operator make sure to use + - * or / operators");
+// }
+// if (operator === "+" || operator === "-"|| operator === "*" || operator === "/")
+// alert(`${a} ${operator} ${b} = ${result}`); 
 
