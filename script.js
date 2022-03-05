9f61a832b863eb92185c4feba42a2c3738d4774e
Omar <omelghobary@gmail.com>
2022-03-06 01:01:05 +0200

    added: multiply functionality

--- script.js
+++ script.js
@@ -1,51 +1,83 @@
 // select buttons 
 const numberButtons = document.querySelectorAll('.number');
 const operationButtons = document.querySelectorAll('.operation');
-const equalsButton = document.querySelector('.equals');
+const equalButton = document.querySelector('.equal');
 const clearButton = document.querySelector('.clear');
-const screen = document.querySelector('.display .screen')
+const deleteButton = document.querySelector('.delete');
+const firstScreen= document.querySelector(' .first-screen');
+const secondScreen = document.querySelector(' .second-screen');
+const tempScreen = document.querySelector('.temp-screen');
+console.log(firstScreen);
+console.log(secondScreen);
+console.log(tempScreen);   
+console.log(equalButton);
 
-for (let i = 0; i < 11; i++){
-    numberButtons[i].addEventListener('click', () => 
-    appendNumber(numberButtons[i].textContent));   
+let numberOneDisplay = '';
+let numberTwoDisplay = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;
+
+numberButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerText === '.' && !haveDot)
+            haveDot = true;
+        else if (e.target.innerText == '.' && haveDot)
+        return;
+        numberTwoDisplay += e.target.innerText;
+        secondScreen.innerText = numberTwoDisplay;
+    });
+});
+
+operationButtons.forEach((button) => {
+    button.addEventListener('click', (e) =>{
+        if (!numberTwoDisplay) return;
+        haveDot = false;
+        const operationName = e.target.innerText;
+        if (numberOneDisplay && numberTwoDisplay && lastOperation){
+            doMath();
+        }
+        else {
+            result = parseFloat(numberTwoDisplay);
+        }
+        clearVar(operationName);
+        lastOperation = operationName;
+    });
+});
+
+function clearVar(name = ''){
+    numberOneDisplay +=  numberTwoDisplay + '' + name +'';
+    firstScreen.innerText = numberOneDisplay;
+    secondScreen.innerText = '';
+    numberTwoDisplay = '';
+    tempScreen.innerText = result;
+
 }
 
-
-function appendNumber(number){
-    if (screen.textContent === "0")
-        clearScreen();
-    screen.textContent += number;
+function doMath(){
+    if (lastOperation = 'x')
+        result = parseFloat(result) * parseFloat(numberTwoDisplay);
 }
 
-function equals (){
-
-}
-
-function operate (operator, num1,num2){
-
-}
-
-function clearScreen(){
-    screen.textContent = "";
-}
-
-
-function add(a,b) {
-    return a+b;
-}
-
-function subtract(a,b) {
-    return a-b;
-}
-
-function multiply(a,b) {
-    return a*b;
-}
-
-function divide(a,b) {
-    return a/b;
-}
  
+// for console
+
+// function add(a,b) {
+//     return a+b;
+// }
+
+// function subtract(a,b) {
+//     return a-b;
+// }
+
+// function multiply(a,b) {
+//     return a*b;
+// }
+
+// function divide(a,b) {
+//     return a/b;
+// }
+
 // let a = +prompt("enter first number");
 // let b = +prompt("enter second number ");
 // let operator = prompt("choose type of operation + - * or /");
